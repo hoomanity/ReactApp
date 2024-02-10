47e77ad939dfed6295ef085c7bdc8bd86ff15914
hooman <87627772+hoomanity@users.noreply.github.com>
2024-02-10 21:28:20 +1100

    installing bootstrap and making a group list

--- ReactApp/src/App.tsx
+++ ReactApp/src/App.tsx
@@ -1,7 +1,10 @@
-import Message from './Message';
-import Mesage from './Message';
+import ListGroup from './components/ListGroup';
 
 function App() {
-  return <div><Message/></div>
+  return (
+    <div>
+      <ListGroup />
+    </div>
+  );
 }
-export default App; 
+export default App;
--- ReactApp/src/Message.tsx
+++ ReactApp/src/Message.tsx
@@ -1,5 +1,6 @@
 function Message() {
     const name = "Hooman"
+    if(name)
     return <h1> Hello {name}</h1>
 }
 export default Message;
